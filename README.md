a79076a91ca956c74fcf6250fd6cd76dc4f84c83
Andy Fragen <andy@thefragens.com>
2020-02-22 08:45:37 -0800

    Quick refactoring
    use Main for testing.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A plugin to safely place for The Events Calendar Category Colors hidden options.
 
-Requires PHP 5.3 as does TEC Category Colors.
+Requires PHP 5.6 as does TEC Category Colors.
--- category-colors-options.php
+++ category-colors-options.php
@@ -1,49 +1,57 @@
 <?php
-
 /**
  * Plugin Name:       Category Colors Options
  * Plugin URI:        https://github.com/afragen/category-colors-options
  * Description:       Testing options for The Events Calendar Category Colors plugin
  * Author:            Andy Fragen
- * Version:           0.3.3
+ * Version:           0.4.0
  * Author URI:        https://github.com/afragen/
  * GitHub Plugin URI: https://github.com/afragen/category-colors-options
- * GitHub Branch:     master
+ * Requires PHP:      5.6
+ * Requires at least: 4.6
  */
-
-add_action( 'plugins_loaded', 'teccc_load_options_class', 20 );
-function teccc_load_options_class() {
-	if ( class_exists( '\\Fragen\\Category_Colors\\Main' ) && class_exists( 'Tribe__Events__Main' ) ) {
-		new Category_Colors_Options();
-	}
-}
 
 class Category_Colors_Options {
 
 	public function __construct() {
 		teccc_add_text_color( 'Red', '#f00' );
 
-		//teccc_ignore_slug( 'just-show-up', 'conference' );
+		// teccc_ignore_slug( 'just-show-up', 'conference' );
 
 		teccc_add_legend_view( 'list' );
-		//teccc_add_legend_view( 'upcoming' );
+		// teccc_add_legend_view( 'upcoming' );
 		teccc_add_legend_view( 'photo' );
 		teccc_add_legend_view( 'week' );
 
-		//teccc_reposition_legend( 'tribe_events_before_footer' );
+		// teccc_reposition_legend( 'tribe_events_before_footer' );
 
-		//teccc_remove_default_legend();
-
-		add_filter( 'teccc_legend_html', array( $this, 'add_legend_explanation' ) );
-		add_action( 'teccc_add_legend_css', array( $this, 'my_legend_css' ) );
-
-	}
-
-	public function add_legend_explanation( $html ) {
-		echo '<div class="legend-explanation"> To focus on events from only one of these categories, just click on the relevant label. </div>' . $html;
-	}
-
-	public function my_legend_css() {
-		echo '#legend li { font-variant: small-caps; }';
+		// teccc_remove_default_legend();
 	}
 }
+
+// Loads Category Colors Options.
+add_action(
+	'plugins_loaded',
+	function() {
+		if ( class_exists( '\\Fragen\\Category_Colors\\Main' ) && class_exists( 'Tribe__Events__Main' ) ) {
+			new Category_Colors_Options();
+		}
+	},
+	20
+);
+
+// Example of 'teccc_legend_html' filter.
+add_filter(
+	'teccc_legend_html',
+	function( $html ) {
+		echo '<div class="legend-explanation"> To focus on events from only one of these categories, jusclick on the relevant label. </div>' . $html;
+	}
+);
+
+// Example of 'teccc_add_legend_css' filter.
+add_action(
+	'teccc_add_legend_css',
+	function() {
+		echo '#legend li { font-variant: small-caps; }';
+	}
+);
